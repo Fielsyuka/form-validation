c58f120354b43b79fa95d7ba8ca588daaf9606b4
fiels <fiels.yuka@gmail.com>
2020-06-01 16:52:09 +0900

    ES6に統一、jQueryを使用。

--- public/js/script.js
+++ public/js/script.js
@@ -1,56 +1,43 @@
-(function(){
 
-	var form = document.getElementById("form");
-	var checkbox = document.getElementById("agree");
-	var btn = document.getElementById("submit");
+// UI
+const form = $('#form');
+const checkbox = $('#agree');
+const btn = $('#submit');
 
-	function Validation(id, label) {
-		this.dom = $(id);
-		this.label = label;
-		this.err = this.dom.next();
+// Function
+const checkAgree = () => {
+	if(checkbox.prop('checked')) {
+		btn.prop('disabled', false);
+	} else {
+		btn.prop('disabled', true);
 	}
+}
 
-	Validation.prototype.checkCount = function() {
-		var text = this.dom.val();
-		if(text.length == 0) {
-			this.addErr();
-			event.preventDefault();
-		} else {
-			this.deleteErr();
-		}
+const checkCount = (id, label) => {
+	const target = $(id);
+	const text = target.val();
+	const errContainer =target.next();
+
+	if(text.length == 0) {
+		errContainer.html(label + 'を入力してください。');
+		target.addClass('err');
+		event.preventDefault();
+	} else {
+		errContainer.html('');
+		target.removeClass('err');
 	}
+}
 
-	Validation.prototype.addErr = function() {
-		this.err.html(this.label + "を入力してください。");
-		this.dom.addClass('err');
-	}
+// Main
+btn.prop('disabled', true);
 
-	Validation.prototype.deleteErr = function() {
-		this.err.html("");
-		this.dom.removeClass('err');
-	}
+checkbox.on('click', function() {
+	checkAgree();
+});
 
-	var checkAgree = function() {
-		if(checkbox.checked) {
-			btn.removeAttribute('disabled');
-		} else {
-			btn.setAttribute('disabled', true);
-		}
-	}
+btn.on('click', function() {
+ checkCount('#name', 'お名前');
+ checkCount('#email', 'メールアドレス');
+ checkCount('#message', '内容');
+});
 
-	btn.setAttribute('disabled', true);
-
-	checkbox.addEventListener('click', function() {
-		checkAgree();
-	});
-
-	btn.addEventListener('click', function() {
-		var validation1 = new Validation("#name", "お名前");
-		var validation2 = new Validation("#email", "メールアドレス");
-		var validation3 = new Validation("#message", "内容");
-		validation1.checkCount();
-		validation2.checkCount();
-		validation3.checkCount();
-	});
-
-})();
